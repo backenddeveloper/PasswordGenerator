f1afbe7eace8451480310744f83787c4ad70456f
Graham Turner <grahamturnernew@hotmail.com>
2017-08-05 22:41:53 +0100

    Updating to include a quiet option.

--- password.py
+++ password.py
@@ -3,34 +3,71 @@
 import sys
 import random
 
-print " \n Graham's password generating script v1.2.1 \n "
 
-if len(sys.argv) == 2:
-    if int(sys.argv[1]) > 2 and int(sys.argv[1]) < 11:
-        password_length = int(sys.argv[1])
-    else:
-        print ' Usage: password.py <password length 3-10>'
+class Password:
+
+    def __init__(self, length):
+        self.wordlist = self.get_wordlist()
+        self.password = []
+        self.generate_password(length)
+
+    def get_wordlist(iself, file='/usr/share/dict/american-english'):
+        with open(file, 'r') as wordlist:
+            return wordlist.read().splitlines()
+
+    def random_index(self):
+        return random.randint(0, len(self.wordlist) - 1)
+
+    def generate_password(self, password_length):
+        for number in range(password_length):
+            self.password.append(self.wordlist[self.random_index()].replace("'", ""))
+
+    def short(self):
+        return ''.join(self.password)
+
+    def long(self):
+        return self.password
+
+
+class Banner:
+
+    @staticmethod
+    def display(password_string, password_array):
+        print ''
+        print " Graham's password generating script v1.3.1 "
+        print ''
+        print ' Your password:    ' + password_string
+        print ''
+        print ' =========================='
+        print ''
+        print 'Easier to remember as:'
+        for element in password_array:
+            print ' ' + element
+        print ''
+
+    @staticmethod
+    def usage():
+        print ''
+        print " Graham's password generating script v1.3.1 "
+        print ''
+        print ' Usage: password.py <password length 3-10> [--quiet]'
         print ' Sadly we only support a password length between 3 and 10 words\n'
-        exit()
-else:
-    print ' Usage: password.py <password length 3-10>\n'
-    exit()
+        print ''
 
-file = open('/usr/share/dict/american-english' , 'r')
-word_list = file.read().splitlines()
 
-password = ''
-password_list = []
+__all__ = ['Password']
 
-for number in range(password_length):
-    random_index = random.randint(0 , len(word_list) - 1)
-    word = word_list[random_index].replace("'" , '')
-    password += word
-    password_list.append(word)
+if __name__ == '__main__':
 
-print 'Your password:    ' +  password + '\n'
-print '=' * 20
-print '\nEasier to remember as: \n'
-for element in password_list:
-    print ' ' + element
-print ''
+    if len(sys.argv) == 3 and int(sys.argv[1]) > 2 and int(sys.argv[1]) < 11 and sys.argv[2] == '--quiet':
+
+        print Password(int(sys.argv[1])).short()
+
+    elif len(sys.argv) == 2 and int(sys.argv[1]) > 2 and int(sys.argv[1]) < 11:
+
+        password = Password(int(sys.argv[1]))
+        Banner.display(password.short(), password.long())
+
+    else:
+
+        Banner.usage()
